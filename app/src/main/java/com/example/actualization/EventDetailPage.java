4efd884968e8c09961beb783cfa086bfae39cc07
Jacob Heiser <dialwolf.windrider@gmail.com>
2022-01-30 21:52:50 -0500

    Event Detail Page activity addon

---
+++ app/src/main/java/com/example/actualization/EventDetailPage.java
@@ -0,0 +1,14 @@
+package com.example.actualization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventDetailPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail_page);
+    }
+}
