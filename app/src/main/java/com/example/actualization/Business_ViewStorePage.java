f2fa70fe1d797f1b41e51f039c7edcf907b50dfe
Jacob <jacobmheiser@gmail.com>
2022-09-13 02:22:19 -0400

    Buisness View Store - Name and Desc

--- app/src/main/java/com/example/actualization/Business_ViewStorePage.java
+++ app/src/main/java/com/example/actualization/Business_ViewStorePage.java
@@ -1,14 +1,88 @@
 package com.example.actualization;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Business_ViewStorePage extends AppCompatActivity {
+    private TextView storeName, storeDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_view_store_page);
+
+        storeName = findViewById(R.id.txtName);
+        storeDescription = findViewById(R.id.txtDescrption);
+
+        //Grab Store Info
+        StoreInformation();
+
+        //Grab Store Appts
+        StoreAppt();
+    }
+
+
+    public void StoreInformation(){
+
+        HashMap<String, String> nameAddresses = new HashMap<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Store Info");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("name").getValue().toString();
+                String desc = snapshot.child("desc").getValue().toString();
+
+                storeName.setText(name);
+                storeDescription.setText(desc);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                throw error.toException();
+            }
+        });
+
+    }
+
+
+    public void StoreAppt(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Store Appointments");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snap: snapshot.getChildren()) {
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
     }
 }
